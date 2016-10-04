0e1698d2f7d8bc845c58e9abf73e0c5dfcd0ed8d
Sam Wemyss <samuel.wemyss@gmail.com>
2016-10-04 16:00:09 +1100

    added google analytics

--- webpack/config.js
+++ webpack/config.js
@@ -29,7 +29,7 @@ export const CONFIG = {
          {
             test: /\.(jpe?g|png|gif|svg)$/i,
             loaders: [
-               'file?hash=sha512&digest=hex&name=[hash].[ext]',
+               'file?hash=sha512&digest=hex&name=bundle-[hash].[ext]',
                'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
             ]
          }
