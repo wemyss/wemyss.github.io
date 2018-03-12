12f327a8e0a7bac54785e7d8a52de1010fc35bb6
Sam Wemyss <samuel.wemyss@gmail.com>
2018-03-12 23:04:09 +1100

    added old about block section for hobbies

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,62 @@
 import React from 'react'
 // import Link from 'gatsby-link'
 
+const Hobbies = () => (
+	<section className="hero is-light is-medium">
+		<HeroHeadTitle title="Hobbies"/>
+		<div className="hero-body">
+			<div className="container">
+				<div className="tile is-ancestor">
+					<div className="tile is-vertical">
+						<div className="tile">
+							<div className="tile is-parent">
+								<article className="tile is-child notification box">
+									<img className="hobby-icon" src={require('../../assets/images/icons/game.svg')}/>
+									<p className="title quantico">Gaming</p>
+									<p className="subtitle">I enjoy unwinding by playing quality games such as The Legend of Zelda and Last of Us (slowly becoming a naughty dog fan...).</p>
+								</article>
+							</div>
+							<div className="tile is-parent">
+								<article className="tile is-child notification box">
+									<img className="hobby-icon" src={require('../../assets/images/icons/mountainBike.svg')}/>
+									<p className="title quantico">Adventuring</p>
+									<p className="subtitle">Whether it be mountain bike riding, bushwalking or simply camping I love the outdoors and exploring what's over the next hill.</p>
+								</article>
+							</div>
+						</div>
+						<div className="tile is-parent">
+							<article className="tile is-child notification box">
+								<div className="columns">
+									<div className="column is-4-tablet is-3-desktop">
+										<img className="hobby-icon" src={require('../../assets/images/icons/bow.svg')}/>
+									</div>
+									<div className="column is-8-tablet is-9-desktop">
+										<p className="title quantico">Archery</p>
+										<p className="subtitle">Wanting to be Legolas as a kid, I picked up a bow and haven't put it down since. Whenever I go home I like to get outdoors and have a shot.</p>
+									</div>
+								</div>
+							</article>
+						</div>
+					</div>
+					<div className="tile is-parent is-vertical">
+						<article className="tile is-child notification box">
+							<img className="hobby-icon" src={require('../../assets/images/icons/plant.svg')}/>
+							<p className="title quantico">Gardening</p>
+							<p className="subtitle">Does this make me sound old..? I grew up surrounded by Mum's garden, and have developed a bit of a green thumb for gardening since moving to Sydney.</p>
+						</article>
+						<article className="tile is-child notification box">
+							<img className="hobby-icon" src={require('../../assets/images/icons/snowboard.svg')}/>
+							<p className="title quantico">Snowboarding</p>
+							<p className="subtitle">Expensive, but loads of fun. I try to make at least one trip to the snow per season to do my annual faceplant while attempting tricks and jumps.</p>
+						</article>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+);
+
+
 const AboutPage = () => (
 	<main>
 		<div className="hero is-primary">
